4f806d9f69108468c2aec236c00be55889719a36
Hendrik Kolassa <hendrik.kolassa@gmail.com>
2016-11-13 01:58:14 +0100

    access change

--- library/src/main/java/com/zarfnates/up2date/FailureReason.java
+++ library/src/main/java/com/zarfnates/up2date/FailureReason.java
@@ -4,7 +4,7 @@ package com.zarfnates.up2date;
  * Created by asantiago on 11/9/16.
  */
 
-final class FailureReason {
+public final class FailureReason {
     static final int NO_PACKAGE_INFO = 0;
     static final int NO_APP_VERSION = 1;
     static final int PLAY_STORE_RESPONSE_ERROR = 2;
